22d0f924e9253f3aeafeecc9f404234e8596dcd5
Edward Gargan <edwgargan@gmail.com>
2017-06-18 22:34:20 +0100

    First commit.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ncurses.h>
+
+int main() {
+
+    initscr();			/* Start curses mode 		  */
+
+    prin
+
+    refresh();			/* Print it on to the real screen */
+    getch();			/* Wait for user input */
+    endwin();			/* End curses mode */
+
+    return 0;
+}
